c44e451be18f4319a74180d2dfd398cd542a9399
Duy Anh NGUYEN <duyanhnguyen08091998@gmail.com>
2024-06-03 14:20:48 +0200

    first commit

---
+++ components/Logo.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Logo = () => {
+  return (
+    <Link href="/">
+      <p className="font-bold text-white bg-primary p-3 rounded-full">.duyanh</p>
+    </Link>
+  );
+};
+
+export default Logo;
